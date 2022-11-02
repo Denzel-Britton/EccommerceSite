542b8f4810f956665ce70fd6dff5c9c2ad8e3092
Denzel-Britton <denzel.britton573@gmail.com>
2022-11-02 17:53:33 -0500

    added cart

--- zeltron/src/Home.js
+++ zeltron/src/Home.js
@@ -13,53 +13,54 @@ function Home() {
 
     <div className="home_row">
      
-    <Product 
-    id=""
-    title= "2022 Air Jordan 4 Retro SE 'Black Canvas"
-     price={431.00}
-    image= {"https://www.snksus.com/pub/media/catalog/product/cache/d07feaef194ff8656e6d5cfff9dd3cf0/a/i/air-jordan-4-black-canvas-dh7138-006-1_1.jpg"}
-    rating={4} />
-
-
     <Product
-    id=""
-    title="Jordan 1 Retro High Chicago 2015 Edition "
-    price={235.00}
-    rating={5}
-    image="https://cdn-images.farfetch-contents.com/13/15/76/97/13157697_21516295_600.jpg"
-    />
-      
-    </div>
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
 
-    <div className="home_row">
-    <Product
-    id=""
-    title="2021
-    Dunk Low 'Black White'"
-    rating={2}
-    image="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/4371ac31-97cd-4057-bd6e-41440e39f3b9/dunk-low-big-kids-shoes-Ss5wx2.png"
-    />
-    {/* <Product
-    id=""
-    title=""
-    rating={}
-    image=""
-    />
-     <Product
-     id=""
-     title=""
-     rating={}
-     image=""
-     />
-    </div>
+        <div className="home__row">
+          <Product
+            id="4903850"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="3254354345"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+        </div>
 
-    <div className="home_row">
-    <Product 
-    id=""
-    title=""
-    rating={}
-    image=""
-    /> */}
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
     
     </div>
 </div>
