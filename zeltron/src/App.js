38457daff3f35a20ad73b9d9528bec8453b19ae9
Denzel-Britton <denzel.britton573@gmail.com>
2022-11-03 11:12:54 -0500

    router setup for the checktout

--- zeltron/src/App.js
+++ zeltron/src/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
 import Home from "./Home";
 function App() {
 return (
+  <Router>
   <div className="app">
-    <Router>
+    
       <Switch>
+      <Route path="/checkout">
+          <Header />
+          <h1>i am the checkout </h1>
+        </Route>
         <Route path="/">
+          <Header />
           <Home />
         </Route>
       </Switch>
+      </div>
     </Router>
-  </div>
+  
 );
 }
 export default App;
