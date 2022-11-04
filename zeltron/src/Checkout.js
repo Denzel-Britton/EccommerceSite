3b298c4d608965ccaf877557a4a9b6429e67bcff
Denzel-Britton <denzel.britton573@gmail.com>
2022-11-04 13:34:09 -0500

    go the subtotal to render on screen using react currency dependencies

--- zeltron/src/Checkout.js
+++ zeltron/src/Checkout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Checkout.css'
+import Subtotal from './Subtotal'
+
 function Checkout() {
   return (
     <div className='checkout'>
@@ -19,7 +21,8 @@ function Checkout() {
         </div>
 
         <div className="cheout_right">
-            <h2>The subtotal goes here</h2>
+            <Subtotal />
+            
         </div>
 
     </div>
