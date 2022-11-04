23eb986639a0390ddf258c9ae034521d4cdc1cb1
Denzel-Britton <denzel.britton573@gmail.com>
2022-11-04 16:14:59 -0500

    added selector to caculate the math for each item

--- zeltron/src/reducer.js
+++ zeltron/src/reducer.js
@@ -1,7 +1,10 @@
 export const initialState = {
     basket: [],
   };
-
+    //a seletor for loop increment and returns
+    export const getBasketTotal = (basket) =>
+    basket?.reduce((amount, item)=> item.price + amount, 0);
+  
   const reducer = (state, action) => {
     console.log(action);
     switch(action.type){
