4ee9397e66ef215eb1665031dc6c613be7c50c4d
Denzel-Britton <denzel.britton573@gmail.com>
2022-11-04 20:47:10 -0500

    added the constat for the email & password good stopping point

--- zeltron/src/Login.js
+++ zeltron/src/Login.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from "react-router-dom";
+
+
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
   return (
     
         <div className='login'>
@@ -16,11 +20,11 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' //value={email} onChange={e => setEmail(e.target.value)} 
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} 
                     />
 
                     <h5>Password</h5>
-                    <input type='password' //value={password} onChange={e => setPassword(e.target.value)} 
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} 
                     />
 
                     <button type='submit' className='login_signInButton'>Sign In</button>
