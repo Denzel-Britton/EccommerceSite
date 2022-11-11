9a52d4592596f222761c223f2e7540fc428b137c
Denzel-Britton <denzel.britton573@gmail.com>
2022-11-10 21:24:43 -0600

    reaseraching how to intergrate stripe

--- zeltron/src/Home.js
+++ zeltron/src/Home.js
@@ -48,7 +48,7 @@ function Home() {
     <div className="home_row">
                 <Product
                 id="4903850"
-                title="Samsung LC49RG90SSUXEN 49 Curved LED Gaming Monitor" 
+                title="Samsung LC49RG90SSUXEN 49 IN. Curved LED Gaming Monitor" 
                 price={199.99} 
                 image={"https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL.AC_SX466_.jpg"} 
                 rating={3}
